5d8603d29bd2fe937dbbaf7b45a095c036ad4cf1
Lukas <tsuki32@hotmail.com>
2021-03-01 18:00:02 +0100

    MainMenu/OptionsMenu
    Prototyped main menu, all elements are there, but its missing astethics. 
    
    Options menu was also added with audiomixers adjusted by sliders for music, enviorment and sfx.

---
+++ Assets/Test_Lukas/Menu Scripts/VolumeSliders.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeSliders : MonoBehaviour
+{
+    public AudioMixer musicMixer;
+    public AudioMixer SFXmixer;
+    public AudioMixer EnviormentMixer;
+
+    public void SetMusicVolume (float volume)
+    {
+        musicMixer.SetFloat("MusicVolume", volume);
+        Debug.Log(volume);
+    }
+
+    public void SetSFXSound(float volume)
+    {
+        SFXmixer.SetFloat("sfxVolume", volume);
+
+        Debug.Log(volume);
+    }
+
+    public void SetEnviormentSound(float volume)
+    {
+        EnviormentMixer.SetFloat("EnviormentSounds", volume);
+
+        Debug.Log(volume);
+    }
+}
