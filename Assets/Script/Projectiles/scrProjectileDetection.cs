91587336df79cda27d4016187bccdb4250de46bb
Jonathan <jonpres@icloud.com>
2021-04-06 16:31:35 +0200

    jobbet litt med projectile movement

---
+++ Assets/Script/Projectiles/scrProjectileDetection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrProjectileDetection : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Get reference and send it to scrTorpedoMovement
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //Loose reference and update scrTorpedoMovement
+    }
+}
