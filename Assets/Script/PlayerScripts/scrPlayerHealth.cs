cae6a565072ed05b97487b3d29ee14ad3b763827
Jonathan <jonpres@icloud.com>
2021-04-14 18:28:14 +0200

    Player can now "die"

---
+++ Assets/Script/PlayerScripts/scrPlayerHealth.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrPlayerHealth : MonoBehaviour
+{
+    [Tooltip("Set player health")]
+    [SerializeField] private int playerHealth;
+    private GameObject playerMesh;
+    private void Awake()
+    {
+        playerMesh = GameObject.FindGameObjectWithTag("PlayerMesh");
+    }
+    private void TakeDamage()
+    {
+        print("ouch!");
+        playerHealth -= 1;
+        if(playerHealth <= 0)
+        {
+            playerDies();
+        }
+    }
+    private void playerDies()
+    {
+        //Dissable player body
+        playerMesh.SetActive(false);
+
+        //Play explotion effect
+        
+        //Move the player to a new scene
+    }
+    private void OnEnable()
+    {
+        scrEnemyProjectiles.OnPlayerDamagedByEnemy += TakeDamage;
+    }
+    private void OnDisable()
+    {
+        scrEnemyProjectiles.OnPlayerDamagedByEnemy -= TakeDamage;
+    }
+}
