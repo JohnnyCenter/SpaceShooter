ee99d96397ce8e7f4116eaa033911f45f31d3641
Lukas <tsuki32@hotmail.com>
2021-04-22 17:21:34 +0200

    Added Lose,Win, end & credits scene

---
+++ Assets/Script/MenuScripts/LoseScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseScreen : MonoBehaviour
+{
+    public void Restart()
+    {
+        Debug.Log("Restarted");
+        SceneManager.LoadScene("Prototype");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("QuitLogged");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Lukas_Prototype");
+    }
+}
