bebbb8ebecd78996d9dc5e19e6785e866ea5f3d3
Lukas <tsuki32@hotmail.com>
2021-04-28 00:31:34 +0200

    Updated all menus and music for menus
    Fikset, font, music for meny, knapper for alle menyer, flyttet menyer til prototype scener, credits har nå en egen knapp på meny.

---
+++ Assets/Test_Lukas/Menu Scripts/AudioManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource[] allAudioSources;
+    public AudioSource inGameMenu;
+    public bool ispaused = false;
+    private void Awake()
+    {
+        allAudioSources = FindObjectsOfType<AudioSource>();
+      
+        
+    }
+   public void stopallaudio()
+   {
+        foreach (AudioSource audioS in allAudioSources)
+        {
+            audioS.Pause();
+        }
+
+        if (ispaused == false)
+        {
+             play();
+
+        }
+
+       if (ispaused == true)
+       {
+
+            inGameMenu.UnPause();
+       }
+
+   }
+
+    public void startAudio()
+    {
+        foreach(AudioSource audioS in allAudioSources)
+        {
+
+         audioS.UnPause();
+        }
+
+        inGameMenu.Pause();
+    }
+
+    void play()
+    {
+        inGameMenu.Play();
+        ispaused = true;
+    }
+
+    
+}
---
+++ Assets/Test_Lukas/Menu Scripts/CreditsAudio.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsAudio : MonoBehaviour
+{
+    public AudioSource creditsgame;
+    private AudioSource[] allAudioSources;
+
+    public void Awake()
+    {
+        allAudioSources = FindObjectsOfType<AudioSource>();
+
+
+    }
+
+        
+    public void playcredit()
+    {
+        creditsgame.Play();
+        foreach (AudioSource audioS in allAudioSources)
+        {
+
+            audioS.Pause();
+        }
+    }
+
+    public void stopPlaying()
+    {
+        creditsgame.Stop();
+
+        foreach (AudioSource audioS in allAudioSources)
+        {
+            audioS.UnPause();
+        }
+
+    }
+
+
+}
+
+
