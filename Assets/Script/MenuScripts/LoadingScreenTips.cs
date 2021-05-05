9b1faa651c4922198e1f78855708102f53cef0cb
Jonathan August Michelsen <jonathan.99@live.no>
2021-05-06 01:06:03 +0200

    LoadingScreenTips and CometSpawner

--- Assets/Script/MenuScripts/LoadingScreenTips.cs
+++ Assets/Script/MenuScripts/LoadingScreenTips.cs
@@ -7,5 +7,39 @@ public class LoadingScreenTips : MonoBehaviour
 {
     [SerializeField]
     TextMeshProUGUI[] Tips;
+    private int tipChosen;
+    private bool tipIsDisplaying;
 
+    private void Awake()
+    {
+       foreach (TextMeshProUGUI tip in Tips)
+        {
+            tip.enabled = false;
+        } 
+
+        ChooseTip();
+        tipIsDisplaying = false;
+    }
+
+    void ChooseTip()
+    {
+        tipChosen = Random.Range(0, 16);
+        Debug.Log("Tip has been chosen, it's " + tipChosen);
+    }
+
+    private void Update()
+    {
+        if (!tipIsDisplaying)
+        {
+            DisplayTip();
+            Debug.Log("Displaying tip");
+        }
+    }
+
+    void DisplayTip()
+    {
+        Tips[tipChosen].enabled = true;
+        tipIsDisplaying = true;
+        Debug.Log("Tip has been displayed");
+    }
 }
---
+++ Assets/Script/Spawner/CometSpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CometSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private List<int> numberPool;
+    private int numberCount;
+
+    [SerializeField]
+    GameObject[] Comets;
+
+    private void Awake()
+    {
+        numberPool = new List<int>();
+        ReloadNumberPool();
+        numberCount = 0;
+    }
+
+    void ReloadNumberPool()
+    {
+        for (int n = 0; n < 4; n++)
+        {
+            numberPool.Add(n);
+        }
+    }
+
+    void PickNumber()
+    {
+        int index = Random.Range(0, numberPool.Count - 1);
+        int i = numberPool[index];
+        numberPool.RemoveAt(index);
+        numberCount += 1;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            PickNumber();
+        }
+
+        if (numberCount == 4)
+        {
+            StartCoroutine("Refill");
+        }
+    }
+
+    IEnumerator Refill()
+    {
+        numberCount = 0;
+        ReloadNumberPool();
+        yield return null;
+    }
+}
