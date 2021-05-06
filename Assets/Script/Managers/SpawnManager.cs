931a0113cd2ba68bb08556881900a997f54d9656
Jonathan August Michelsen <jonathan.99@live.no>
2021-05-06 12:26:59 +0200

    LES DESCRIPTION!
    Minimap har blitt bugga og nå roterer med spilleren! Måner og spacestations har blitt plassert ut der jeg ønsker å ha de når jeg builder så hvis dere flytter på dem så flytt den tilbake.

---
+++ Assets/Script/Managers/SpawnManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    scrCircleSpawner CircleSpawner;
+
+    private void Awake()
+    {
+        CircleSpawner = FindObjectOfType<scrCircleSpawner>();
+    }
+
+    private void Start()
+    {
+        CircleSpawner.gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        Moon.PlayerEnterMoonZoneGreen += ActivateCircleSpawner;
+        Moon.PlayerEnterMoonZoneRed += ActivateCircleSpawner;
+        Moon.PlayerEnterMoonZoneYellow += ActivateCircleSpawner;
+        Moon.PlayerExitMoonZone += DeactivateCircleSpawner;
+    }
+
+    private void OnDisable()
+    {
+        Moon.PlayerEnterMoonZoneGreen -= ActivateCircleSpawner;
+        Moon.PlayerEnterMoonZoneRed -= ActivateCircleSpawner;
+        Moon.PlayerEnterMoonZoneYellow -= ActivateCircleSpawner;
+        Moon.PlayerExitMoonZone -= DeactivateCircleSpawner;
+    }
+
+    void ActivateCircleSpawner()
+    {
+        CircleSpawner.gameObject.SetActive(true);
+    }
+
+    void DeactivateCircleSpawner()
+    {
+        CircleSpawner.gameObject.SetActive(false);
+    }
+}
